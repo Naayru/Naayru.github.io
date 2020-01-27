e0b39a857abfb1fce85547ae36758d1c87dcf39b
OlivierCharon <oli.charon@gmail.com>
2020-01-27 17:17:24 +0100

    form front updated

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,7 +1,35 @@
-jQuery(document).ready(function($) {
-	$('#illustration').hover( function(){
-		$(this).addClass('expand');
-	}, function(){
-		$(this).removeClass('expand');
-	} );
+$(document).ready(function ($) {
+    $('#switch').click(function () {
+        if ($('#sign-up-div').is(":visible")) {
+            $('#sign-up-div').fadeOut(function () {
+                $('#sign-in-div').fadeIn();
+            });
+        } else if($('#sign-in-div').is(":visible")) {
+            $('#sign-in-div').fadeOut(function() {
+                $('#sign-up-div').fadeIn();
+            });
+        }
+    });
+    /*
+        $('#sign-up').click(function () {
+            $('#sign-in-div').fadeOut( , function() {
+                $('#sign-up-div').fadeIn();
+            });
+            console.log('sign up');
+        })
+
+     */
 });
+
+var choices = $('#switch .choice')
+    , text = $('#switch .or')
+
+choices
+    .on('click', function () {
+        choices.toggleClass('on')
+
+        text.addClass('flip')
+        setTimeout(function () {
+            text.removeClass('flip')
+        }, 1000)
+    })
